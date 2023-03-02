7f515506408d029be1dd4e71b08255af077303e5
elitemagic <toddmerlin@hotmail.com>
2023-03-02 13:49:31 +1100

    Updated for 2.8

--- index.js
+++ index.js
@@ -1,269 +1,328 @@
-const http = require('http'),
-    express = require('express'),
-    url = require('url'),
-    morgan = require('morgan'),
-    path = require('path'),
-    fs = require('fs'),
+const express = require('express'),
     bodyParser = require('body-parser'),
-    uuid = require('uuid'),
-    app = express();
+    uuid = require('uuid');
 
-app.use(bodyParser.json());
+const morgan = require('morgan');
+const app = express();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Modals.Directors;
 
-let users = [
-    {
-        id: '1',
-        name: 'Nathan',
-        favoriteMovies: []
-    },
-    {
-        id: '2',
-        name: 'Andrew',
-        favoriteMovies: ["Nope"]
-    }
-];
+mongoose.connect('mongodb://localhost:27017/cfDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ });
+
+ app.use(bodyParser.json());
+
+ app.use(morgan("common"));
 
 
-let movies = [
-    {
-      Title: 'Tommy Boy',
-      Director: {
-        Name: 'Peter Segal',
-        Bio: 'Blurb'
-      },
-      MainActor: 'Chris Farley',
-      Genre: {
-        Name: 'Comedy',
-        Description: 'Funny'
-      }
-    },
-    {
-        Title: 'Office Space',
-        Director: {
-            Name: 'Mike Judge',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Ron Livingston',
-        Genre: {
-            Name: 'Comedy',
-            Description: 'Funny'
-        }
-    },
-    {
-        Title: 'The Notebook',
-        Director: {
-            Name: 'Nick Cassavetes',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Rachel McAdams',
-        Genre: {
-            Name: 'Drama',
-            Description: 'More serious'
-        }
-    },
-    {
-        Title: 'The Godfather',
-        Director: {
-            Name: 'Francis Ford Coppola',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Al Pacino',
-        Genre: {
-            Name: 'Drama',
-            Description: 'More serious'
-        }
-    },
-    {
-        Title: 'The Matrix',
-        Director: {
-            Name: 'The Wachowskis',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Keanu Reeves',
-        Genre: {
-            Name: 'Action',
-            Description: 'Exciting'
-        }
-    },
-    {
-        Title: 'Terminator 2',
-        Director: {
-            Name: 'James Cameron',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Arnold Schwarzenegger',
-        Genre: {
-            Name: 'Action',
-            Description: 'Exciting'
-        }
-    },
-    {
-        Title: 'A nightmare on Elm Street',
-        Director: {
-            Name: 'Wes Craven',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Robert Englund',
-        Genre: {
-            Name: 'Horror',
-            Description: 'Scary'
-        }
-    },
-    {
-        Title: 'Nope',
-        Director: {
-            Name: 'Jordan Peele',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Daniel Kaluuya',
-        Genre: {
-            Name: 'Horror',
-            Description: 'Scary'
-        }
-    },
-    {
-        Title: 'Elf',
-        Director: {
-            Name: 'Jon Favreau',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Will Ferrell',
-        Genre: {
-            Name: 'Christmas',
-            Description: 'In December'
-        }
-    },
-    {
-        Title: 'Die Hard',
-        Director: {
-            Name: 'John McTiernan',
-            Bio: 'Blurb'
-          },
-        MainActor: 'Bruce Willis',
-        Genre: {
-            Name: 'Christmas',
-            Description: 'In December'
-        }
-    }
-];
+    // path = require('path'),
+    // fs = require('fs'),
+    
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+
+
+// let users = [
+//     {
+//         id: '1',
+//         name: 'Nathan',
+//         favoriteMovies: []
+//     },
+//     {
+//         id: '2',
+//         name: 'Andrew',
+//         favoriteMovies: ["Nope"]
+//     }
+// ];
+
+
+// let movies = [
+//     {
+//       Title: 'Tommy Boy',
+//       Director: {
+//         Name: 'Peter Segal  ',
+//         Bio: 'Blurb'
+//       },
+//       MainActor: 'Chris Farley',
+//       Genre: {
+//         Name: 'Comedy',
+//         Description: 'Funny'
+//       }
+//     },
+//     {
+//         Title: 'Office Space',
+//         Director: {
+//             Name: 'Mike Judge',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Ron Livingston',
+//         Genre: {
+//             Name: 'Comedy',
+//             Description: 'Funny'
+//         }
+//     },
+//     {
+//         Title: 'The Notebook',
+//         Director: {
+//             Name: 'Nick Cassavetes',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Rachel McAdams',
+//         Genre: {
+//             Name: 'Drama',
+//             Description: 'More serious'
+//         }
+//     },
+//     {
+//         Title: 'The Godfather',
+//         Director: {
+//             Name: 'Francis Ford Coppola',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Al Pacino',
+//         Genre: {
+//             Name: 'Drama',
+//             Description: 'More serious'
+//         }
+//     },
+//     {
+//         Title: 'Avatar',
+//         Director: {
+//             Name: 'James Cameron',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Zoe Saldana',
+//         Genre: {
+//             Name: 'Action',
+//             Description: 'Exciting'
+//         }
+//     },
+//     {
+//         Title: 'Terminator 2',
+//         Director: {
+//             Name: 'James Cameron',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Arnold Schwarzenegger',
+//         Genre: {
+//             Name: 'Action',
+//             Description: 'Exciting'
+//         }
+//     },
+//     {
+//         Title: 'A nightmare on Elm Street',
+//         Director: {
+//             Name: 'Wes Craven',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Robert Englund',
+//         Genre: {
+//             Name: 'Horror',
+//             Description: 'Scary'
+//         }
+//     },
+//     {
+//         Title: 'Nope',
+//         Director: {
+//             Name: 'Jordan Peele',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Daniel Kaluuya',
+//         Genre: {
+//             Name: 'Horror',
+//             Description: 'Scary'
+//         }
+//     },
+//     {
+//         Title: 'Elf',
+//         Director: {
+//             Name: 'Jon Favreau',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Will Ferrell',
+//         Genre: {
+//             Name: 'Christmas',
+//             Description: 'In December'
+//         }
+//     },
+//     {
+//         Title: 'Die Hard',
+//         Director: {
+//             Name: 'John McTiernan',
+//             Bio: 'Blurb'
+//           },
+//         MainActor: 'Bruce Willis',
+//         Genre: {
+//             Name: 'Christmas',
+//             Description: 'In December'
+//         }
+//     }
+// ];
 
 
 // READ
 
-app.get('/movies', (req, res) => {
-    res.status(200).json(movies)
+// app.get('/movies', (req, res) => {
+//     res.status(200).json(movies)
+// });
+
+// app.get('/movies/:title', (req, res) => {
+//     // const title = req.params.title;
+//     const { title } = req.params;
+//     const movie =movies.find ( movie => movie.Title === title );
+//     if (movie) {
+//         res.status(200).json(movie);
+//     } else {
+//         res.status(400).send(`no such movie`)
+//     }
+// });
+
+// app.get('/movies/genre/:genreName', (req, res) => {
+//     // const title = req.params.title;
+//     const { genreName } = req.params;
+//     const genre =movies.find ( movie => movie.Genre.Name === genreName ).Genre;
+//     if (genre) {
+//         res.status(200).json(genre);
+//     } else {
+//         res.status(400).send(`no such genre`)
+//     }
+// });
+
+// app.get('/movies/directors/:directorName', (req, res) => {
+//     const { directorName } = req.params;
+//     const director =movies.find ( movie => movie.Director.Name === directorName ).Director;
+
+//     if (director) {
+//         res.status(200).json(director);
+//     } else {
+//         res.status(400).send(`no such director`)
+//     }
+// });
+
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
+
+app.get("/", (req, res) => {
+    res.send("Welcome to myFlix!");
 });
 
-app.get('/movies/:title', (req, res) => {
-    // const title = req.params.title;
-    const { title } = req.params;
-    const movie =movies.find ( movie => movie.Title === title );
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(400).send(`no such movie`)
-    }
-});
 
-app.get('/movies/genre/:genreName', (req, res) => {
-    // const title = req.params.title;
-    const { genreName } = req.params;
-    const genre =movies.find ( movie => movie.Genre.Name === genreName ).Genre;
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send(`no such genre`)
-    }
-});
-
-app.get('/movies/directors/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director =movies.find ( movie => movie.Director.Name === directorName ).Director;
-
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send(`no such director`)
-    }
-});
+app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 
 // Create
 
-app.post('/users', (req, res) => {
-    const newUser = req.body;
+//app.post('/users', (req, res) => {
+//     const newUser = req.body;
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send(`users need names`)
-    }
-});
+//     if (newUser.name) {
+//         newUser.id = uuid.v4();
+//         users.push(newUser);
+//         res.status(201).json(newUser)
+//     } else {
+//         res.status(400).send(`users need names`)
+//     }
+// });
 
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
+// app.post('/users/:id/:movieTitle', (req, res) => {
+//     const { id, movieTitle } = req.params;
     
-    let user = users.find(user => user.id == id );
+//     let user = users.find(user => user.id == id );
 
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`)
-    } else {
-        res.status(400).send(`Users favorite movie not added`)
-    }
-});
+//     if (user) {
+//         user.favoriteMovies.push(movieTitle);
+//         res.status(200).send(`${movieTitle} has been added to user ${id}'s array`)
+//     } else {
+//         res.status(400).send(`Users favorite movie not added`)
+//     }
+// });
 
 
 // UPDATE
 
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
+// app.put('/users/:id', (req, res) => {
+//     const { id } = req.params;
+//     const updatedUser = req.body;
 
-    let user = users.find(user => user.id == id );
+//     let user = users.find(user => user.id == id );
 
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send(`User has not been updated`)
-    }
-});
+//     if (user) {
+//         user.name = updatedUser.name;
+//         res.status(200).json(user);
+//     } else {
+//         res.status(400).send(`User has not been updated`)
+//     }
+// });
 
 
 // DELETE
 
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
+// app.delete('/users/:id/:movieTitle', (req, res) => {
+//     const { id, movieTitle } = req.params;
     
-    let user = users.find(user => user.id == id );
+//     let user = users.find(user => user.id == id );
 
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+//     if (user) {
+//         user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
 
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`)
+//         res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`)
         
-    } else {
-        res.status(400).send(`Users favorite movie was not deleted`)
-    }
-});
+//     } else {
+//         res.status(400).send(`Users favorite movie was not deleted`)
+//     }
+// });
 
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
+// app.delete('/users/:id', (req, res) => {
+//     const { id } = req.params;
     
-    let user = users.find(user => user.id == id );
+//     let user = users.find(user => user.id == id );
 
-    if (user) {
-        users = users.filter( user => user.id != id);
-        res.status(200).send(` User ${id} has been deleted`)
-    } else {
-        res.status(400).send(`user not deleted`)
-    }
-});
+//     if (user) {
+//         users = users.filter( user => user.id != id);
+//         res.status(200).send(` User ${id} has been deleted`)
+//     } else {
+//         res.status(400).send(`user not deleted`)
+//     }
+// });
 
 
 
@@ -282,9 +341,9 @@ app.delete('/users/:id', (req, res) => {
 // });
 
 
-app.use(morgan('combined', { stream: accessLogStream }));
+// app.use(morgan('combined', { stream: accessLogStream }));
 
-
+app.use("/documentation", express.static("public"));
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
