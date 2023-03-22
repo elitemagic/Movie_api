c63c4e5498c7309edb0bc6f1c92ae02d5d098d8e
elitemagic <toddmerlin@hotmail.com>
2023-03-22 13:52:45 +1100

    2.10

--- passport.js
+++ passport.js
@@ -12,21 +12,26 @@ passport.use(new LocalStrategy({
   passwordField: 'Password'
 }, (username, password, callback) => {
   console.log(username + '  ' + password);
-  Users.findOne({ Username: username })
-  .then((user) => {
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+
     if (!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      return callback(null, false, {message: 'Incorrect username.'});
     }
+
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'});
+    }
+
     console.log('finished');
     return callback(null, user);
-  })
-  .catch((err) => {
-    console.log(err);
-    return callback(err, false);
   });
-})
-);
+}));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
