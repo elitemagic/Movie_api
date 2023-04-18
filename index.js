360910df010a906b4c0b8342f69e0fedbaa19c86
elitemagic <toddmerlin@hotmail.com>
2023-04-18 14:09:53 +1000

    f

--- index.js
+++ index.js
@@ -44,12 +44,11 @@ const { check, validationResult } = require('express-validator');
 
 // Cors middleware allowing all Cross Origin Requests
 const cors = require('cors');
+app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
 
 
 
