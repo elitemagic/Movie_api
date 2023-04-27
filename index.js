140c19787882aee5ef6f8cabfc6185872b4e9acb
elitemagic <toddmerlin@hotmail.com>
2023-04-27 12:19:38 +1000

    fr

--- index.js
+++ index.js
@@ -29,7 +29,18 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 // Cors middleware allowing all Cross Origin Requests
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:3000', 'http://localhost:1234', 'https://my-flix-service.onrender.com'];
+
+app.use(cors({
+ origin: (origin, callback) => {
+   if(!origin) return callback(null, true);
+   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+     return callback(new Error(message ), false);
+   }
+   return callback(null, true);
+ }
+}));
 
 
 // Logging
